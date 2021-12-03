0e0d49e881d194dfbd77fce46daaa7b9f24696f0
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-03 03:25:49 -0600

    pushing code to github

--- server.js
+++ server.js
@@ -4,3 +4,9 @@ const morgan = require("morgan");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use(morgan("dev"));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static.apply("public"));
