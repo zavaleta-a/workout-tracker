37ad3ae7960847ede379a090f03430104b4ab415
Fredo <84590140+zavaleta-a@users.noreply.github.com>
2021-12-04 12:12:06 -0600

    added routes under htmlRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -18,4 +18,34 @@ module.exports = function (app) {
         res.json(err);
       });
   });
+
+  app.get("/api/wourkouts/range", function (req, res) {
+    Workout.find()
+      .then((data) => {
+        res.json(data);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts/range", function (req, res) {
+    Workout.create({})
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.put("/api/workouts/:id", ({ body, params }, res) => {
+    Workout.findByIdAndUpdate(
+      params.id,
+      { $push: { exercise: body } },
+      { new: true, runValidators: true }
+    )
+      .then((data) => res.json(data))
+      .catch((err) => {
+        res.json(err);
+      });
+  });
 };
